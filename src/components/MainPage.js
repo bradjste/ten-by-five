44e89ec834163401794abfcb08c459538f4b2a9a
Brad Stevenson <bradjste@gmail.com>
2021-04-07 12:27:25 -0500

    Fixed deploy bugs

--- src/components/MainPage.js
+++ src/components/MainPage.js
@@ -11,9 +11,9 @@ class MainPage extends Component {
     render() {
         return(
             <div className = 'landing-page-div'>
-                <img src={title} className='main-banner'></img>
+                <img src={title} alt='title graphic' className='main-banner'></img>
                 <br />
-                <img src={subtitle} className='main-subheader'></img>
+                <img src={subtitle} alt='subtitle graphic' className='main-subheader'></img>
                 <PanelGrid />
                 <About />
             </div>
