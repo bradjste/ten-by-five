99de55330fa2dc54c960491d2cee236e210adc1d
Brad Stevenson <bradjste@gmail.com>
2021-04-30 17:01:26 -0500

    Updated panel grid and git gateway config change

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
 import MainPage from './components/MainPage';
+import { IdentityContextProvider } from 'react-netlify-identity';
 import './App.css';
 
 function App() {
+  const url = 'https://your-identity-instance.netlify.com/'; // supply the url of your Netlify site instance. VERY IMPORTANT. no point putting in env var since this is public anyway
+
   return (
     <div className="App">
       <MainPage />
--- src/components/PanelGrid.js
+++ src/components/PanelGrid.js
@@ -1,11 +1,21 @@
 import React, {Component} from 'react';
+import Panel from './Panel'
+
 
 class PanelGrid extends Component {
 
     render() {
+        const items = [];
+
+        for (let r = 1; r < 6; r++) {
+            for (let c = 1; c < 11; c++) {
+                items.push(<Panel key={(c+r*10)-10} col={c} row={r}></Panel>)
+            }
+        }
+
         return (
             <div className='panel-grid-div'>
-                PANEL GRID
+                {items}
             </div>
         )
     }
