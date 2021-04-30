1bd313304acc57e95e3e90ef84b2de7a0d670258
Brad Stevenson <bradjste@gmail.com>
2021-04-30 17:06:47 -0500

    Forgot the other error

--- src/App.js
+++ src/App.js
@@ -2,8 +2,6 @@ import MainPage from './components/MainPage';
 import './App.css';
 
 function App() {
-  const url = 'https://your-identity-instance.netlify.com/'; // supply the url of your Netlify site instance. VERY IMPORTANT. no point putting in env var since this is public anyway
-
   return (
     <div className="App">
       <MainPage />
