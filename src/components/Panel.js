e7ce1579f526ef8ac4caa8005e1e191058cce22d
Brad Stevenson <bradjste@gmail.com>
2021-04-30 17:51:22 -0500

    CMS refactoring and what not

--- src/components/Panel.js
+++ src/components/Panel.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
+import img from '../../public/images/1st-Render-1.png';
 
 class Panel extends Component {
 
     render() {
         return(
             <div className = 'panel-div'>
-                {"This is panel (" + this.props.row + "," + this.props.col + ")"}
+                <img alt={"Panel (" + this.props.row + "," + this.props.col + ")"} src={'../../public/img/1st-Render-' + this.props.key + '.png'}/>                
             </div>
         )
     }
