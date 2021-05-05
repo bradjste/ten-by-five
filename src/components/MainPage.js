64cd947dcfbee99bf88bb1f1f8ff21959b4991d1
Brad Stevenson <bradjste@gmail.com>
2021-05-05 17:14:04 -0500

    Added lottie test

--- src/components/MainPage.js
+++ src/components/MainPage.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Player, Controls } from '@lottiefiles/react-lottie-player';
 import title from '../img/banner.png';
 import subtitle from '../img/subheader.png';
 import PanelGrid from './PanelGrid.js';
@@ -7,6 +8,7 @@ import About from './About.js';
 class MainPage extends Component {
 
     render() {
+        const numbers = [0,1,2,3,4]
         return(
             <div id='main-page-div'>
                 <img src={title} alt='title graphic' className='main-banner'/>
@@ -14,6 +16,16 @@ class MainPage extends Component {
                 <img src={subtitle} alt='subtitle graphic' className='main-subheader'></img>
                 <PanelGrid openPanelDetail={this.props.openPanelDetail}/>
                 <About />
+                <div style={{'display':'flex','flexDirection':'row','paddingLeft':'0px'}}>
+                    {numbers.map((number) =>
+                        <Player
+                        autoplay
+                        loop
+                        src="https://assets5.lottiefiles.com/private_files/lf30_kanwuonz.json"
+                        style={{ height: '300px', width: '300px' }}/>
+                    )}
+                </div>
+                
                 <br/>
             </div>
         )
