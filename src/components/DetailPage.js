770e671a954f5f7fa55f0b089d3a041bcac651ba
Brad Stevenson <bradjste@gmail.com>
2021-08-12 18:24:13 -0500

    Finished detail page element alignment

--- src/components/DetailPage.js
+++ src/components/DetailPage.js
@@ -6,6 +6,8 @@ import titleSig from '../img/IMG-3_Proprieter.png';
 import titlePlacard from '../img/V-GROUP_Placard.svg';
 import titleStuff from '../img/V-GROUP_Static-1.svg';
 import panelFrame from '../img/V-GROUP_Panel.svg';
+import secondRender from '../img/IMG-5_2nd-Render.png';
+import thirdRender from '../img/IMG-4_3rd-Render.png';
 import swatchA from '../img/IMG-8_Swatch-A.png';
 import swatchB from '../img/IMG-8_Swatch-B.png';
 import swatchC from '../img/IMG-8_Swatch-C.png';
@@ -18,6 +20,31 @@ import priceIcon from '../img/V_Switch-Inactive.svg';
 import assetValue from '../img/V-13_Asset-Value.svg';
 import printValue from '../img/V-12_Print-Value.svg';
 import centerFrame from '../img/V-GROUP_Ribbon.svg';
+import swatchBracket from '../img/V-9_Bracket.svg';
+import paletteWheel from '../img/V-9_Subject-Ratio.svg';
+import paletteLine from '../img/V-14_Color-Bar.svg';
+import bubble from '../img/V-10_Bubble.svg';
+import wineUp from '../img/V-10_Theme-1.svg';
+import wineDown from '../img/V-10_Theme-2.svg';
+import centerTag2 from '../img/V-9_Label.svg';
+import centerTag1 from '../img/V-10_Label.svg';
+import castOuter from '../img/V-11_Icon.svg';
+import cast1 from '../img/V-11_Cast-1.svg';
+import cast2 from '../img/V-11_Cast-2.svg';
+import cast1Hover from '../img/V-11_Cast-1-Hover.svg';
+import cast2Hover from '../img/V-11_Cast-2-Hover.svg';
+import castBaby from '../img/V-11_Baby.svg';
+import fourthRenderBox from '../img/V-15_4th-R-Frame.svg';
+import fourthRenderLabel from '../img/V-15_Label.svg';
+import fourthRender from '../img/IMG-15_4th-Render.png';
+import ratingTag from '../img/V-16_Tag.svg';
+import rating from '../img/IMG-16_Penmanship.png';
+
+
+
+
+
+
 
 
 
@@ -91,8 +118,11 @@ class DetailPage extends Component {
                         </div>
                     </div>
                     <div id="panel-col">
-                        <img id="panel-frame" alt="panel frame" src={panelFrame}/>
-        
+                        <div style={{position: 'relative'}}>
+                            <img id="panel-frame" alt="panel frame" src={panelFrame}/>
+                            <img style={{width:'380px',position: 'absolute',top:"15px",left:"74px",filter:"blur(0.8px)"}} alt="second render" src={secondRender}/>
+                            <img style={{width:'380px',position: 'absolute',top:"15px",left:"74px"}}  alt="third render" src={thirdRender}/>
+                        </div>
                     </div>
                     <div id="stats-col">
                         <div id='upper-block'>
@@ -119,6 +149,17 @@ class DetailPage extends Component {
                                     <div>
                                         <div style={{position:'relative'}}>
                                             <img style={{width:'180px',position: 'absolute',left:"-20px",top:"100px"}} alt="center frame" src={centerFrame}/>
+                                            <img style={{width:'45px',position: 'absolute',left:"-60px",top:"10px"}} alt="swatch grouping" src={swatchBracket}/>
+                                            <img style={{width:'90px',position: 'absolute',left:"10px",top:"123px"}} alt="palette wheel" src={paletteWheel}/>
+                                            <img style={{width:'90px',position: 'absolute',left:"162px",top:"159px"}} alt="character study box" src={centerTag1}/>
+                                            <img style={{width:'120px',position: 'absolute',left:"58px",top:"91px"}} alt="themes box" src={centerTag2}/>
+                                            <img style={{width:'70px',position: 'absolute',left:"120px",top:"190px"}} alt="wine holder" src={bubble}/>
+                                            <img style={{width:'20px',position: 'absolute',left:"136px",top:"197px"}} alt="wine up" src={wineUp}/>
+                                            <img style={{width:'20px',position: 'absolute',left:"210px",top:"197px"}} alt="wine down" src={wineDown}/>
+                                            <img style={{width:'50px',position: 'absolute',left:"10px",top:"35px"}} alt="cast outer" src={castOuter}/>
+                                            <img style={{width:'30px',position: 'absolute',left:"80px",top:"38px"}} alt="cast 1" src={cast1}/>
+                                            <img style={{width:'27px',position: 'absolute',left:"120px",top:"38px"}} alt="cast 2" src={cast2}/>
+                                            <img style={{width:'80px',position: 'absolute',left:"160px",top:"8px"}} alt="cast baby" src={castBaby}/>
                                         </div>
                                     </div>
                                 </div>
@@ -141,7 +182,17 @@ class DetailPage extends Component {
                             </div>          
                         </div>
                         <div id='lower-block'>
-
+                            <img style={{width:'66px',position: 'relative',top:'135px',left:'-38px'}} alt="dotted line" src={dottedSwatchLine}/>
+                            <div style={{display: "flex"}}>
+                                <img style={{marginRight: '15px'}} className="price-icon" alt='palette icon' src={priceIcon}/>
+                                <img alt="pallete line" src={paletteLine}/>
+                            </div>
+                            <img style={{width:'195px',position: 'absolute',top:'55px',left:'27px'}} alt="fourth render box" src={fourthRenderBox}/>
+                            <img style={{width:'193px',position: 'relative',top:'22px',left:'28px'}} alt="fourth render" src={fourthRender}/>
+                            <img style={{width:'110px',position: 'absolute',top:'80px',left:'225px'}} alt="fourth render tag" src={fourthRenderLabel}/>
+                            <img style={{width:'66px',position: 'absolute',top:'153px',left:'223px'}} alt="dotted line" src={dottedSwatchLine}/>
+                            <img style={{width:'70px',position: 'absolute',top:'130px',left:'290px'}} alt="rating box" src={ratingTag}/>
+                            <img style={{width:'70px',position: 'absolute',top:'120px',left:'300px'}} alt="rating" src={rating}/>
                         </div>
                     </div>
                 </div>
